a6956dd1a47a95a7d0f97c19e9cf5f70eca8271b
Phelipe <phelipeuni@gmail.com>
2017-05-24 09:11:54 -0300

    aprimoramento na coloração do heatmap

--- views/_js/mapa.js
+++ views/_js/mapa.js
@@ -2,28 +2,21 @@ var map, heatmap, infoWindow;
 var gradient = [
 	'rgba(255, 0  , 0  , 0)', //Resto do mapa
 
-	'rgba(255, 0  , 0  , 1)', //Vermelho
-	'rgba(255, 25 , 0  , 1)',
-	'rgba(255, 50 , 0  , 1)',
-	'rgba(255, 75 , 0  , 1)',
-	'rgba(255, 100, 0  , 1)',
-	'rgba(255, 125, 0  , 1)',
-	'rgba(255, 150, 0  , 1)',
-	'rgba(255, 175, 0  , 1)',
-	'rgba(255, 200, 0  , 1)',
-	'rgba(255, 225, 0  , 1)',
+	'rgba(255, 0  , 0  , 0.67)', //Vermelho
+	'rgba(255, 45 , 0  , 0.70)',
+	'rgba(255, 90 , 0  , 0.73)',
+	'rgba(255, 150, 0  , 0.76)',
+	'rgba(255, 195, 0  , 0.79)',
+	'rgba(255, 225, 0  , 0.82)',
 
-	'rgba(255, 255, 0  , 1)', //Amarelo
 
-	'rgba(225, 255, 0  , 1)',
-	'rgba(200, 255, 0  , 1)',
-	'rgba(175, 255, 0  , 1)',
-	'rgba(150, 255, 0  , 1)',
-	'rgba(125, 255, 0  , 1)',
-	'rgba(100, 255, 0  , 1)',
-	'rgba(75 , 255, 0  , 1)',
-	'rgba(50 , 255, 0  , 1)',
-	'rgba(25 , 255, 0  , 1)',
+	'rgba(255, 255, 0  , 0.70)', //Amarelo
+
+	'rgba(225, 255, 0  , 0.85)',
+	'rgba(195, 255, 0  , 0.88)',
+	'rgba(150 , 255, 0  , 0.91)',
+	'rgba(90 , 255, 0  , 0.94)',
+	'rgba(45 , 255, 0  , 0.97)',
 	'rgba(0  , 255, 0  , 1)'  //Verde
 ]
 
@@ -31,7 +24,7 @@ function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
 		
 		center: {lat: -15.763, lng: -47.869},
-		zoom: 18,
+		zoom: 16,
 		mapTypeControl: false,
 		streetViewControl: false
 	
@@ -48,7 +41,7 @@ function initMap() {
 
 			infoWindow.setPosition(pos);
 			infoWindow.setContent('Localização Encontrada.');
-			map.setCenter(pos);
+			
 		}, function() {
 			handleLocationError(true, infoWindow, map.getCenter());
 		});
@@ -57,12 +50,13 @@ function initMap() {
 	}
 
 	heatmap = new google.maps.visualization.HeatmapLayer({
+		
 		data: getPoints(),
-		radius: 200,
+		radius: 50,
 		gradient: gradient,
-		opacity: 0.2,
-		maxIntensity: 10,
+		opacity: 0.4,		
 		map: map
+
 	});
 }
 
@@ -77,12 +71,12 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 
 function getPoints() {
 	return [
-			{location: new google.maps.LatLng(-15.76070, -47.87050), weight: 30},
-			{location: new google.maps.LatLng(-15.76150, -47.87030), weight: 10},
-			{location: new google.maps.LatLng(-15.76230, -47.87010), weight: 10},
-			{location: new google.maps.LatLng(-15.76310, -47.86980), weight: 10},
-			{location: new google.maps.LatLng(-15.76390, -47.86930), weight: 10},
-			{location: new google.maps.LatLng(-15.76470, -47.86830), weight: 10},
-			{location: new google.maps.LatLng(-15.76550, -47.86690), weight: 10},
+			{location: new google.maps.LatLng(-15.76070, -47.87050), weight: 0},
+			{location: new google.maps.LatLng(-15.76150, -47.87030), weight: 0},
+			{location: new google.maps.LatLng(-15.76230, -47.87010), weight: 0},
+			{location: new google.maps.LatLng(-15.76310, -47.86980), weight: 0},
+			{location: new google.maps.LatLng(-15.76390, -47.86930), weight: 0},
+			{location: new google.maps.LatLng(-15.76470, -47.86830), weight: 0},
+			{location: new google.maps.LatLng(-15.76550, -47.86690), weight: 0},
 		];
 }
