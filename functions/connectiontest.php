647984c62d11752be2d8c6b9f529fa86c2fea2df
Edudacosta <educostao.1050@gmail.com>
2017-05-29 16:45:18 -0300

    Pasta de parametrização.

--- functions/connectiontest.php
+++ functions/connectiontest.php
@@ -6,6 +6,6 @@
 	$packetloss = checkPacketLoss('127.0.0.1', 40);
 	$connetion_intensity = checkIntensity();
 	
-	echo "<b>Perda de Pacote : </b>", $packetloss, "%<br>";
+	echo "<b> Perda de Pacote : </b>", $packetloss, "%<br>";
 	echo "<b>Intensidade do Sinal: </b>", $connetion_intensity;
 ?>
