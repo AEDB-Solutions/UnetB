b1eccc51d1bbbc59085a91e67c02f64fb4089b8d
Edudacosta <educostao.1050@gmail.com>
2017-05-30 16:05:06 -0300

    Alterações na parametrização

--- funcoes_de_parametrizacao/intensity_param.php
+++ funcoes_de_parametrizacao/intensity_param.php
@@ -1,12 +1,15 @@
-<?php 
+<?php
+	require_once "../functions/intensidade.php"; 
 	function intensity_param($intensidade){
 		if ($intensidade<-70)
 		{
 			return 0;
-			//exit;
-		};
-		if ($intensidade>0){
+		}
+		else if ($intensidade>0){ 
 			return 10;
+		}
+		else{
+			return ((0.14285714285) * $intensidade)+10;
 		}
 	}
 ?>
