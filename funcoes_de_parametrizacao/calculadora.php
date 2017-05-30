f0683b235f5f634f677dbf85f5523a1a9b1cd349
victorsanp <victorpi20@hotmail.com>
2017-05-29 22:55:16 -0300

    pequenas correcoes

--- funcoes_de_parametrizacao/calculadora.php
+++ funcoes_de_parametrizacao/calculadora.php
@@ -6,11 +6,18 @@ require_once "../funcoes_de_parametrizacao/parametro_download.php";
 require_once "../funcoes_de_parametrizacao/parametro_upload.php";
 require_once "../classes/class-UnetbDB.php";
 
-function connect();
+if(connect())
+{
+	echo "Conectado ao banco de dados";
+}
+	else{
+		echo "Erro ao conectar ao banco de dados";
+	}
 
 //selecionando dados da tabela
 $sql = "SELECT * FROM functions"
 $query = mysql_query($sql);
+
 while($sql = mysql_fetch_array($query))
 	{
 	$intensidade = $sql["intensidade"]; //$intensidade é a variavel que rerpresenta a coluna "intensidade" da tabela
@@ -18,13 +25,12 @@ while($sql = mysql_fetch_array($query))
 	$download = $sql["download_speed"];
 	$upload = $sql["upload_speed"];
 
-echo "$nome"; //exibindo o que foi achado na coluna "nome".
 	}
 
-	$n_intensidade = function parametro_intensidade($intensidade);
-	$n_latencia = function parametro_latencia($latencia);
-	$n_download = function parametro_download($download);
-	$n_upload = function parametro_upload($upload);
+	$n_intensidade = parametro_intensidade($intensidade); // parametro_intensidade eh a funcao que parametriza
+	$n_latencia = parametro_latencia($latencia);
+	$n_download = parametro_download($download);
+	$n_upload = parametro_upload($upload);
 
 	$media_parametros = ((3.5 * $n_intensidade) + (2.5 * $n_download) + (2.5 * $n_upload) + (1.5 * $n_latencia)) / 10;
 
