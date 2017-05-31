b1353319034cda8948faabbadeac340fdbab3356
Edudacosta <educostao.1050@gmail.com>
2017-05-31 11:56:22 -0300

    Parametrização perda de pacotes

--- funcoes_de_parametrizacao/packetloss_param.php
+++ funcoes_de_parametrizacao/packetloss_param.php
@@ -1,10 +1,5 @@
 <?php
-	require_once "../functions/packetloss.php";
-
 	function param_packet($porcentagem){
 		return $porcentagem/10;
 	}
-	$teste = checkPacketLoss('164.41.4.26', 40);
-	echo "Parametro: ",param_packet($teste) ,"- Porcentagem: ", $teste, "%"; 
-
 ?>
