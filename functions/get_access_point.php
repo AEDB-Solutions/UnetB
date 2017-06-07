ff6dbeefeed83eea1c2e1c27c22b91db87e7f520
zerbinidamata <rafinhazerbini@gmail.com>
2017-06-07 15:04:54 -0300

    att get_acess_point

--- functions/get_access_point.php
+++ functions/get_access_point.php
@@ -4,7 +4,7 @@
 
 		$command = 'iwconfig | grep "Access Point"';
 		$result = shell_exec($command);
-		return substr($result, 59, 17);
+		return substr($result, 57, 17);
 	}
 	// $meu_mac = get_access_point();
 	// var_dump($meu_mac);
