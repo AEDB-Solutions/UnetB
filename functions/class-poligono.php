a8c496f4a806dad235cb7f7739de184cc4bce14a
danieldeamorim <danieldeamorimaguiar@hotmail.com>
2017-06-13 16:07:46 -0300

    testando se ponto está na UNB

--- functions/class-poligono.php
+++ functions/class-poligono.php
@@ -1,7 +1,7 @@
 <?php
 
     class pointLocation {
-        var $pointOnVertex = true; // Checar si el punto se encuentra exactamente en uno de los vértices?
+        var $pointOnVertex = true; // Checar se o ponto se encontra exatamente em um dos vértices?
  
     function pointLocation() {
     }
@@ -9,31 +9,35 @@
         function pointInPolygon($point, $polygon, $pointOnVertex = true) {
         $this->pointOnVertex = $pointOnVertex;
  
-        // Transformar la cadena de coordenadas en matrices con valores "x" e "y"
+        // Transformar as coordenadas em matrizes com valores "x" e "y"
         $point = $this->pointStringToCoordinates($point);
         $vertices = array(); 
         foreach ($polygon as $vertex) {
             $vertices[] = $this->pointStringToCoordinates($vertex); 
         }
  
-        // Checar si el punto se encuentra exactamente en un vértice
+        // Checar se o ponto se encontra exatamente em um vértice
         if ($this->pointOnVertex == true and $this->pointOnVertex($point, $vertices) == true) {
             return "Vertice";
         }
  
-        // Checar si el punto está adentro del poligono o en el borde
+        //  Checar se o ponto está dentro do poligono o em uma borda
         $intersections = 0; 
         $vertices_count = count($vertices);
  
         for ($i=1; $i < $vertices_count; $i++) {
             $vertex1 = $vertices[$i-1]; 
             $vertex2 = $vertices[$i];
-            if ($vertex1['y'] == $vertex2['y'] and $vertex1['y'] == $point['y'] and $point['x'] > min($vertex1['x'], $vertex2['x']) and $point['x'] < max($vertex1['x'], $vertex2['x'])) { // Checar si el punto está en un segmento horizontal
+            if ($vertex1['y'] == $vertex2['y'] and $vertex1['y'] == $point['y'] and $point['x'] > min($vertex1['x'], $vertex2['x']) and $point['x'] < max($vertex1['x'], $vertex2['x'])) 
+
+		{ // Checar se o ponto está em um segmento horizontal
                 return "Borda";
             }
             if ($point['y'] > min($vertex1['y'], $vertex2['y']) and $point['y'] <= max($vertex1['y'], $vertex2['y']) and $point['x'] <= max($vertex1['x'], $vertex2['x']) and $vertex1['y'] != $vertex2['y']) { 
                 $xinters = ($point['y'] - $vertex1['y']) * ($vertex2['x'] - $vertex1['x']) / ($vertex2['y'] - $vertex1['y']) + $vertex1['x']; 
-                if ($xinters == $point['x']) { // Checar si el punto está en un segmento (otro que horizontal)
+                if ($xinters == $point['x']) 
+
+		{ //  Checar se o ponto está em um segmento (sem ser o horizontal)
                     return "Borda";
                 }
                 if ($vertex1['x'] == $vertex2['x'] || $point['x'] <= $xinters) {
@@ -41,13 +45,14 @@
                 }
             } 
         } 
-        // Si el número de intersecciones es impar, el punto está dentro del poligono. 
+        // Se o número de intercessões é impar, o ponto está dentro do polígono. 
         if ($intersections % 2 != 0) {
             return "Dentro";
         } else {
             return "Fora";
         }
-    }
+    
+	}
  
     function pointOnVertex($point, $vertices) {
         foreach($vertices as $vertex) {
@@ -56,7 +61,8 @@
             }
         }
  
-    }
+    
+	}
  
     function pointStringToCoordinates($pointString) {
         $coordinates = explode(" ", $pointString);
--- functions/verificarquadrantes.php
+++ functions/verificarquadrantes.php
@@ -3,7 +3,7 @@
 	require_once "../classes/class-UnetbDB.php";
 	require_once "../functions/class-poligono.php";
 
-		$mySQL = new MySQL;
+	$mySQL = new MySQL;
 	$executaQuery = $mySQL->executeQuery("SELECT * FROM networking_data");
 	$mySQL->disconnect();
 
@@ -16,7 +16,8 @@
  
 	$pointLocation = new pointLocation();
 	$points = array("$x $y");
-	$polygon = array("-15.776999 -47.871338","-15.770020, -47.850267","-15.749136, -47.867133","-15.753349, -47.880951", "-15.776999 -47.871338");
+	$polygon = array("-15.760218 -47.878049","-15.754683 -47.859552","-15.772484 -47.849467","-15.777481 -47.872470", "-15.760218 -47.878049");
+	
 	// Las últimas coordenadas tienen que ser las mismas que las primeras, para "cerrar el círculo"
 	foreach($points as $key => $point) {
     echo "point " . ($key+1) . " ($point): " . $pointLocation->pointInPolygon($point, $polygon) . "<br>";
