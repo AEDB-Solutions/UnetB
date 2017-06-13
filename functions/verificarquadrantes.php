aeb58ab71818adba2aea912ccfda69491867e36e
danieldeamorim <danieldeamorimaguiar@hotmail.com>
2017-06-13 16:27:48 -0300

    testando se ponto está na UNB

--- functions/verificarquadrantes.php
+++ functions/verificarquadrantes.php
@@ -18,7 +18,7 @@
 	$points = array("$x $y");
 	$polygon = array("-15.760218 -47.878049","-15.754683 -47.859552","-15.772484 -47.849467","-15.777481 -47.872470", "-15.760218 -47.878049");
 	
-	// Las últimas coordenadas tienen que ser las mismas que las primeras, para "cerrar el círculo"
+	// As últimas coordenadas tem que ser as mesmas das primeiras para fechar o poligono
 	foreach($points as $key => $point) {
     echo "point $id ($point): " . $pointLocation->pointInPolygon($point, $polygon) . "<br>";
    		}
