53cf5840d952b4960216d9ab4ba4897df7b0a834
zerbinidamata <rafinhazerbini@gmail.com>
2017-06-21 10:49:33 -0300

    Get ssid

--- functions/get_ssid.php
+++ functions/get_ssid.php
@@ -1,11 +1,9 @@
 <?php
- function get_ssid()
- {
-
-
-	$command = 'iwgetid';
-	$result = shell_exec($command);
-	$ssid = substr($result, strpos($result, 'ESSID:')+7, -2);
-	return ssid;
- } 
+ 	function get_ssid()
+ 	{
+	 $command = 'iwgetid';
+	 $result = shell_exec($command);
+	 $ssid = substr($result, strpos($result, 'ESSID:')+7, -2);
+	 return ssid;
+	} 
 ?>
