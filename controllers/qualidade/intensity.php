f57105a772b91686dd4c83f69c324d9d4a776bc1
Phelipe <phelipeuni@gmail.com>
2017-06-21 19:47:47 -0300

    pequenas alterações

--- controllers/qualidade/intensity.php
+++ controllers/qualidade/intensity.php
@@ -2,11 +2,9 @@
 
 	require_once "../../functions/intensity.php";      //arquivo calcula intensidade
 	$intensity = checkIntensity();
-	$intensityLevel = intensityLevel();
 
 	$parametros = array(
 		"intensity"  => "$intensity",
-		"nivel" => "$intensityLevel",
 	);
 	echo json_encode($parametros);
 ?>
